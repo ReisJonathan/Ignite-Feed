f0080f6eaeb4d302b80a4d1086f8acc37c874662
Jonathan Reis <jonathanreis7834@gmail.com>
2024-05-15 17:48:09 -0300

    feat: Cria componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import {Post} from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
+
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Jonathan Reis"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quam vero cumque pariatur, provident libero unde alias quibusdam reprehenderit facere quo quidem quasi molestiae iste assumenda est corrupti nesciunt inventore?"
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Novo post"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Jonathan Reis"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quam vero cumque pariatur, provident libero unde alias quibusdam reprehenderit facere quo quidem quasi molestiae iste assumenda est corrupti nesciunt inventore?"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Novo post"
+          />
+        </main>
+      </div>
+      
     </div>
   )
 }
